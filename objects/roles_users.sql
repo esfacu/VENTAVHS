215cf56c18444b98d75322fb71388bc7e8434430
esfacu <88353287+esfacu@users.noreply.github.com>
2024-05-19 22:33:25 +0000

    arreglada error en permisos

--- objects/roles_users.sql
+++ objects/roles_users.sql
@@ -1,12 +1,11 @@
 USE VENTA_DE_PELICULAS;
 
--- Crear usuarios
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password_admin';
-CREATE USER 'user'@'localhost' IDENTIFIED BY 'password_user';
-CREATE USER 'facundo.rodriguez@pedidosya.com' IDENTIFIED BY '123456';
+-- Crear usuarios si no existen
+CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'password_admin';
+CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'password_user';
+CREATE USER IF NOT EXISTS 'facundo.rodriguez@pedidosya.com' IDENTIFIED BY '123456';
 
 -- Asignar privilegios a los usuarios
 GRANT SELECT, INSERT, UPDATE, DELETE ON * TO 'facundo.rodriguez@pedidosya.com';
 GRANT SELECT, INSERT, UPDATE, DELETE ON * TO 'admin'@'localhost';
 GRANT SELECT ON * TO 'user'@'localhost';
-
